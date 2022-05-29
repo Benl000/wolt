f404aff6f45c9b449ff86255a33908cd23de0b9b
Ben <Benl000100@gmail.com>
2022-05-29 16:55:08 +0300

    header design

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,37 @@
 import { Component } from 'react';
+import woltLogo from '../assets/svgs/woltLogo.svg';
+import profileLogo from '../assets/svgs/profile.svg';
+import arrowLogo from '../assets/svgs/arrow_header_btn.svg';
 
 export default class Header extends Component {
     render() {
         return (
             <header className='header'>
                 <section className='header-main'>
-                    <h1>Wolt</h1>
-                    <input type="text" placeholder='Search in Wolt...' />
-                    <nav>
-                        <button>Log in</button>
-                        <button className='signup-btn'>Sign up</button>
-                        <button>English
-                            {/* <svg arrow/> */}
-                        </button>
-                        <button>👤
-                            {/* <svg profile+arrow/> */}
+                    <section className='header-main-logo'>
+                        <img className='logo' src={woltLogo} />
+                    </section>
+                    <section className='header-main-search'>
+                        <input type="text" placeholder='Search in Wolt...' />
+                    </section>
+                    <nav className='header-main-nav'>
+                        <div>
+                            <button>Log in</button>
+                            <button className='signup-btn'>
+                                <span>
+                                    Sign up
+                                </span>
+                            </button>
+                            <button className='lang-btn'>
+                                <span>
+                                    English
+                                </span>
+                                <img className='arrowLogo' src={arrowLogo} />
+                            </button>
+                        </div>
+                        <button className='profile-btn'>
+                            <img className='profileLogo' src={profileLogo} />
+                            <img className='arrowLogo' src={arrowLogo} />
                         </button>
                     </nav>
                 </section>
