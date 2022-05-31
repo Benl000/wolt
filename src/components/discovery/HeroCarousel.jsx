3ac7f9b80b30682e116fbbf8284981ba8852d0ba
Tali <taligavrielov@gmail.com>
2022-05-31 11:51:18 +0300

    ignore carousel status

--- src/components/discovery/HeroCarousel.jsx
+++ src/components/discovery/HeroCarousel.jsx
@@ -69,6 +69,7 @@ export const HeroCarousel = () => {
         <Carousel 
         style={{position: 'relative'}}
         showIndicators={false}
+        showStatus={false}
         infiniteLoop
 
         renderArrowPrev={(onClickHandler) => {
