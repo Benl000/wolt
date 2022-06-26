efc920bfb0d437cbe8983114debb4fbb1a691463
Ben <Benl000100@gmail.com>
2022-06-26 20:46:16 +0300

    discovery arrows

--- src/components/discovery/RestaurantsCarouselList.jsx
+++ src/components/discovery/RestaurantsCarouselList.jsx
@@ -2,9 +2,23 @@ import { RestaurantsCarouselPreview } from './RestaurantsCarouselPreview';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
+import React from 'react';
+import { useState } from 'react';
+import { CarouselNavButtons } from './buttons';
 
 export const RestaurantsCarouselList = ({ restaurants }) => {
-    var settings = {
+
+    const customeSlider = React.createRef();
+
+    const gotoNext = () => {
+        customeSlider.current.slickNext();
+    };
+
+    const gotoPrev = () => {
+        customeSlider.current.slickPrev();
+    };
+
+    const [sliderSettings, setSliderSettings] = useState({
         dots: false,
         infinite: false,
         slidesToShow: 4,
@@ -24,13 +38,15 @@ export const RestaurantsCarouselList = ({ restaurants }) => {
                 }
             },
         ]
-    };
+    });
+
     return (
         <section className="restaurant-list">
             <div className="restaurant-list-header">
                 <h1>Restaurants</h1>
+                <CarouselNavButtons preSlide={() => gotoPrev()} nextButton={() => gotoNext()} />
             </div>
-            <Slider className="restaurant-list-slider" {...settings}>
+            <Slider className="restaurant-list-slider"  {...sliderSettings} ref={customeSlider}>
                 {restaurants && restaurants.map(restaurant =>
                     <RestaurantsCarouselPreview key={restaurant.id} restaurant={restaurant} />)}
             </Slider>
